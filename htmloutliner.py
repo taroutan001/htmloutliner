5e782f6a9bf30fa94f79e6fcb8669ba3143f162b
taroutan001 <taroutan001@gmail.com>
2013-08-22 00:09:51 +0900

    HTML Parser class Added

--- htmloutliner.py
+++ htmloutliner.py
@@ -2,6 +2,33 @@
 # -*- coding: utf-8 -*-
 
 import urllib2
+from HTMLParser import HTMLParser
+
+
+class CommonHTMLParser(HTMLParser):
+
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.tab  = ""
+        self.val  = None
+        self.data = []
+
+    def handle_starttag(self,tagname,attributes):
+
+        print self.tab + "<" + tagname + ">"
+
+        self.tab += "\t"
+        for attr in attributes:
+            tmptmp = attr
+            print self.tab + "@" + attr[0] + "=" + attr[1]
+
+    def handle_endtag(self,tagname):
+        self.tab = self.tab[:-1]
+        print self.tab + "</" +tagname + ">"
+
+    def handle_data(self,data):
+        print ""
+
 
 def output_outline(url):
     raw_html = ""
@@ -17,12 +44,21 @@ def output_outline(url):
 if __name__ == "__main__":
 
     print u"HTML解析開始"
+    url = "http://www.google.co.jp"
+    opened_url = urllib2.urlopen(url)
+    raw_html = opened_url.read()
 
-    outline = output_outline("http://www.google.co.jp")
+    parser = CommonHTMLParser()
+    parser.feed(raw_html)
 
-    print u"----HTML START----"
-    print outline
-    print u"----HTML END----"
+    parser.close()
+
+    #outline = output_outline("http://www.google.co.jp")
+
+
+    #print u"----HTML START----"
+    #print outline
+    #print u"----HTML END----"
 
     print u"HTML解析終了"
 
